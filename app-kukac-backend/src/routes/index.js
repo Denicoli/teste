353311c6cd1a1ac9645ced71f9cd646bcbae67d7
Denicoli <tiagodenicoli@hotmail.com>
2020-02-15 21:32:30 -0300

    first commit

---
+++ app-kukac-backend/src/routes/index.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const router = express.Router();
+router.get('/', function (req, res, next) {
+    res.status(200).send({
+        title: "Node Express API",
+        version: "0.0.1"
+    });
+});
+router.post('/teste', function (req, res, next) {
+    res.status(200).send({
+        title: "Node Express API"
+    });
+});
+module.exports = router;
